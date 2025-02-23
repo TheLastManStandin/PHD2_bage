531fbb2e99b9784001c8fe80c2f3bb87193fc5f8
moondog <cahisabeat@gmail.com>
2025-02-23 19:51:25 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import neopixel
 import time
+import ntptime
 from wifi import connect_wifi
 from config import load_env
 from paint import paint_this
 
-paint_this("burger")
+paint_this("amogus")
 
 config = load_env()
 
 ssid = config.get("WIFI_SSID")
 password = config.get("WIFI_PASS")
 wlan = connect_wifi(ssid, password)
+
+try:
+    ntptime.settime()
+except Exception as e:
+    print("Ошибка синхронизации времени:", e)  
+
+current_time = time.localtime()
+print("Текущее время:", current_time)
